1af016c25f7fa91e010a3f29ce80df7f7409c6c0
muffpy <rahulathreya@yahoo.com>
2024-07-04 23:39:27 +0100

    Add rpc definitions

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -22,8 +22,21 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+type WorkerArgs struct {
+	MapTaskNumber    int // finished maptask number
+	ReduceTaskNumber int // finished reducetask number
+}
 
+type WorkerReply struct {
+	Tasktype int // 0: map task, 1: reduce task, 2: waiting, 3: job finished
+	NMap     int // number of map task
+	NReduce  int // number of reduce task
+
+	MapTaskNumber int    // map task only
+	Filename      string // maptask only
+
+	ReduceTaskNumber int // reducetask only
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
